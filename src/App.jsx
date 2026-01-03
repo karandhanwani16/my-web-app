4047ec326c5357ab5455a19dedc91fd9434b29a5
Karan Dhanwani <karandhanwani20@gmail.com>
2026-01-03 19:09:11 +0530

    added list

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ const App = () => {
   return (
     <div>
       <div>My Web App</div>
-      <div>Checking Build Process</div>
-      <div>Checking Build Process</div>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
     </div>
   )
 }
